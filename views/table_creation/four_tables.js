79f582e49f4776519db988ef1e21ad201eb1b415
apoorv-asc <apoorvsingh.chauhan2019@vitstudent.ac.in>
2022-01-23 20:42:36 +0530

    Table creation problem handled

--- views/table_creation/four_tables.js
+++ views/table_creation/four_tables.js
@@ -22,29 +22,30 @@ app.get('/',function(req,res){
         if(err)
         console.log(err);
         else
-        res.send("CUSTOMERS table has been added");
+        console.log("CUSTOMERS table has been added");
     });
     let sql2='CREATE TABLE medicines(id int AUTO_INCREMENT, med_name varchar(255), company_name varchar(255),price decimal(10,2), quantity int, PRIMARY KEY(id))';
     db.query(sql2,function(err,result){
         if(err)
         console.log(err);
         else
-        res.send("medicines table has been added");
+        console.log("medicines table has been added");
     });
     let sql3='CREATE TABLE companies(id int AUTO_INCREMENT, username varchar(255), company varchar(255),email varchar(255), password varchar(255), PRIMARY KEY(id))';
     db.query(sql3,function(err,result){
         if(err)
         console.log(err);
         else
-        res.send("COMPANIES table has been added");
+        console.log("COMPANIES table has been added");
     });
     let sql4='CREATE TABLE bought_medicines(id int AUTO_INCREMENT, username varchar(255), company varchar(255),med_name varchar(255), quantity int, price decimal(10,2),bill decimal(10,2), PRIMARY KEY(id))';
     db.query(sql4,function(err,result){
         if(err)
         console.log(err);
         else
-        res.send("boutght_medicines table has been added");
+        console.log("boutght_medicines table has been added");
     });
+    res.send('All four tables were created.\n\nCheck phpmyadmin');
 })
 
 app.listen(27017,process.env.IP,function(){
